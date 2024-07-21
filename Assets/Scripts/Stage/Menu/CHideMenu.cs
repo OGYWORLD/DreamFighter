7de0e8b7ac700cc758271ceee63a95160fe5dda6
OZY <ogy1004@naver.com>
2024-07-22 03:36:06 +0900

    Add Region

--- Assets/Scripts/Stage/Menu/CHideMenu.cs
+++ Assets/Scripts/Stage/Menu/CHideMenu.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#region ¿À°¡À»
+#endregion
+
 public class CHideMenu : MonoBehaviour
 {
     public GameObject menuInfo;
